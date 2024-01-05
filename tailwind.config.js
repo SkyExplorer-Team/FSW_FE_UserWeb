f6e145245dbdb6cac6e587f53381c8fa4229011d
Riani BM <rianimatialu@gmail.com>
2024-01-05 08:54:27 +0700

    gambar, warna (temporary)

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+            primary: {
+                100: "rgba(214, 250, 230, 1)",
+                200: "rgba(175, 246, 212, 1)",
+                300: "rgba(131, 229, 191, 1)",
+                400: "rgba(96, 203, 172, 1)",
+                500: "rgba(56, 169, 147, 1)",
+                600: "rgba(45, 145, 135, 1)",
+                700: "rgba(34, 120, 121, 1)",
+                800: "rgba(24, 88, 97, 1)",
+                900: "rgba(16, 66, 80, 1)",
+              },
+          main:{
+            primary: rgba(56, 169, 147, 1),
+            secondary: '#ffed4a',
+            danger: 'rgba(200, 26, 35, 1)',
+            success: 'rgba(63, 162, 68, 1)',
+            base_black: rgba(17, 17, 17, 1),
+            black: rgba(53, 64, 83, 1),
+            outline: rgba(242, 244, 247, 1),
+            neutral: rgba(103, 112, 132, 1)
+          },
+          disabled: {
+            DEFAULT: "rgba(208, 213, 221, 1)",
+          },
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
+  
